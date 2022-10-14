ee8e79a65149f752c151cd7667cbb01876af4e4b
Victor Abrokwah <kingportipher@outlook.com>
2022-10-13 22:17:50 -0700

    Task 4 done successfully

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,16 @@
 /**
  * Canada
  *
- * */
+ *
+ */
 int main(void)
 {
+	/**
+	 * Returns output
+	 */
 	puts("\"Programming is like building a multilingual puzzle");
+	/**
+	 * Returns 0
+	 */
 	return (0);
 }
