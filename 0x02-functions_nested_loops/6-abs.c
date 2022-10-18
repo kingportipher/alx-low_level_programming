73882494bdc4a222d0352962e2cb6a684dc9fce2
Victor Abrokwah <kingportipher@outlook.com>
2022-10-18 09:48:32 -0700

    Task 6 done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+i#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @t: The integer to be computed
+ * Return: The absolute value
+ */
+int _abs(int)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
