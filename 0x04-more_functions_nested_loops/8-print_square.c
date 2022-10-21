94290c52248fae881a4705e5f5f33040b275b6af
Victor Abrokwah <kingportipher@outlook.com>
2022-10-20 22:25:00 -0700

    Task 8 done

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square -prints a square
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int lenght;
+		int breadth;
+
+		for (length = 0; length < size; lenght++)
+		{
+			for (breadth = 0; breadth < size; breadth++)
+			{
+				_putchar('a');
+			}
+			_putchar('\n');
+		}
+	}
+}
