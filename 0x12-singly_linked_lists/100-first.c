7666148ab4ef070632597fdfd5fdb678dd812bdd
Victor Abrokwah <kingportipher@outlook.com>
2022-11-24 14:46:27 -0800

    Advance task 100 completed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void_attribute_((constructor)) hare(void);
+
+/**
+ * hare - Prints a string
+ * before the main function is executed.
+ */
+void hare(void)
+{
+	printf("You're best! and yet, you must allow,"
+			"\nI bore my house upon my back!\n");
+}
