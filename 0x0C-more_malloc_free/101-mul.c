1fcd428cb348b537dd5b22554776d84dec1eff1c
Victor Abrokwah <kingportipher@outlook.com>
2023-11-25 11:16:24 -0800

    Task 100 fix

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _print - moves a string one place to the left and
+ * prints the string
+ * @str: string to move
+ * @1: size of string
+ *
+ * Return: void
+ */
+void _print(char *str, int 1)
+{
+
